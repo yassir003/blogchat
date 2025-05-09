fdfb0f4d3516980a8a1443ccc2535a664d354ebc
yassir003 <[moulzimelalaoui@gmail.com]>
2025-05-09 02:16:16 +0100

    add markdown to create post form and show post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -29,6 +30,7 @@ class PostController extends Controller
     }
 
     public function showSinglePost(Post $post) {
-    return view('single-post',['post' => $post]);
+        $post['body'] = Str::markdown($post->body);
+        return view('single-post',['post' => $post]);
     }
 }
